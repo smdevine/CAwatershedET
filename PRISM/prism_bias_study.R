d6fe23eea7fe03ffc41bca9d2933d6be931e1e89
smdevine <smdevine@ucdavis.edu>
2017-05-15 16:41:52 -0700

    Completed section to quantify temporal trends in the orographic effect by watershed inherent in PRISM

--- PRISM/prism_bias_study.R
+++ PRISM/prism_bias_study.R
@@ -1,4 +1,5 @@
 library(raster)
+library(Kendall)
 prism_historical <- 'D:/PRISM_ppt_stable_4kmM2_189501_198012_bil'
 prism_modern <- 'D:/PRISM_ppt_stable_4kmM3_198101_201609_bil' #updated 5.12.2017 with WY2016 precip data
 californiaDir <- 'C:/Users/smdevine/Desktop/SpatialData/CA_counties/government_units'
@@ -6,6 +7,7 @@ PRISMdir <- 'C:/Users/smdevine/Desktop/SpatialData/PRISM/PRISM_CA_ppt_stable'
 boundariesDIR <- file.path('C:/Users/smdevine/Desktop/SpatialData/watershed_characteristics/watershed_boundaries_revised/final_boundaries_CA_TA_dissolved')
 demDir <- 'C:/Users/smdevine/Desktop/SpatialData/PRISM/DEM'
 P_vs_elevDir <- 'C:/Users/smdevine/Desktop/SpatialData/PRISM/results/bias_analysis/P_vs_elev_by_watershed'
+rivername_converter <- 'C:/Users/smdevine/Desktop/SpatialData/PRISM/watershed_to_rivername.csv'
 
 #put historical montly 4km prism files into a raster stack and save to file
 setwd(prism_historical)
@@ -86,7 +88,59 @@ for (i in 2:length(watershed_fnames)) {
   write.csv(watershed_P_vs_elev, paste(watershed_name, '_P_vs_elev.csv', sep=''), row.names = FALSE)
 }
 
+#read in p vs. elev files
+setwd(P_vs_elevDir)
+p_vs_elev_fnames <- list.files()
+p_vs_elev <- lapply(p_vs_elev_fnames, read.csv, stringsAsFactors=FALSE)
+p_vs_elev_fnames <- as.data.frame(p_vs_elev_fnames)
+colnames(p_vs_elev_fnames) <- 'filenames'
+p_vs_elev_fnames$watershed_name <- gsub('_P_vs_elev.csv', '', p_vs_elev_fnames$filenames)
+rivernames <- read.csv(rivername_converter, stringsAsFactors = FALSE)
+p_vs_elev_fnames <- merge(p_vs_elev_fnames, rivernames, by='watershed_name')
+names(p_vs_elev) <- p_vs_elev_fnames$river_name
+for (i in 1:length(p_vs_elev)) {
+  #rivername <- names(p_vs_elev)[i]
+  if (i == 1) {
+    for (j in 3:ncol(p_vs_elev[[i]])) {
+      if (j == 3) {
+        lm_p_vs_elev <- summary(lm(p_vs_elev[[i]][,j] ~ p_vs_elev[[i]][,2])) #j is in reference to the sequential columns of years, 2 is in reference to elevation column
+        slopes_P_vs_elev <- cbind(as.integer(gsub("WY.", "", colnames(p_vs_elev[[i]])[j])), round(lm_p_vs_elev$coefficients[2], 5))
+        #colnames(slopes_P_vs_elev) <- c('water.year',  names(p_vs_elev)[i])
+        next
+      }
+      lm_p_vs_elev <- summary(lm(p_vs_elev[[i]][,j] ~ p_vs_elev[[i]][,2]))
+      slopes_P_vs_elev <- rbind(slopes_P_vs_elev, cbind(as.integer(gsub("WY.", "", colnames(p_vs_elev[[i]])[j])), round(lm_p_vs_elev$coefficients[2], 5)))
+    }
+    next
+  }
+  for (j in 3:ncol(p_vs_elev[[i]])) {
+    if (j==3) {
+      lm_p_vs_elev <- summary(lm(p_vs_elev[[i]][,j] ~ p_vs_elev[[i]][,2]))
+      slopes_P_vs_elev_add <- round(lm_p_vs_elev$coefficients[2], 5)
+      next
+    }
+    lm_p_vs_elev <- summary(lm(p_vs_elev[[i]][,j] ~ p_vs_elev[[i]][,2]))
+    slopes_P_vs_elev_add <- c(slopes_P_vs_elev_add, round(lm_p_vs_elev$coefficients[2], 5))
+  }
+  slopes_P_vs_elev <- cbind(slopes_P_vs_elev, slopes_P_vs_elev_add)
+}
+slopes_P_vs_elev <- as.data.frame(slopes_P_vs_elev)
+colnames(slopes_P_vs_elev) <- c('water.year', names(p_vs_elev))
+setwd(file.path(P_vs_elevDir, 'orographic_trends'))
+write.csv(slopes_P_vs_elev, 'orographic_slopes_1896_2016.csv', row.names = FALSE)
 
+#read in results to calculate temporal trends by watershed with variable start and stop times
+setwd(file.path(P_vs_elevDir, 'orographic_trends'))
+slopes_P_vs_elev <- read.csv('orographic_slopes_1896_2016.csv', stringsAsFactors = FALSE)
+mktest_allyrs <- apply(slopes_P_vs_elev[ ,2:21], 2, MannKendall)
+mktest_1905_2016 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1905):nrow(slopes_P_vs_elev), 2:21], 2, MannKendall)
+mktest_1920_2016 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1920):nrow(slopes_P_vs_elev), 2:21], 2, MannKendall)
+mktest_1950_2016 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1950):nrow(slopes_P_vs_elev), 2:21], 2, MannKendall)
+mktest_1970_2016 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1970):nrow(slopes_P_vs_elev), 2:21], 2, MannKendall)
+mktest_1980_2016 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1980):nrow(slopes_P_vs_elev), 2:21], 2, MannKendall)
+mktest_1970_2010 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1970):which(slopes_P_vs_elev$water.year==2010), 2:21], 2, MannKendall)
+mktest_1950_2010 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1950):which(slopes_P_vs_elev$water.year==2010), 2:21], 2, MannKendall)
+mktest_1920_2010 <- apply(slopes_P_vs_elev[which(slopes_P_vs_elev$water.year==1920):which(slopes_P_vs_elev$water.year==2010), 2:21], 2, MannKendall)
 
 #test watershed plotting on PRISM data
 setwd(boundariesDIR)
